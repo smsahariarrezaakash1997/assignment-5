8cfff9438edf444515c55f2b2ebcc9dd82197c81
smsahariarrezaakash1997 <developerakash50@gmail.com>
2024-09-30 23:48:00 +0600

    history done

--- js/noakhli.js
+++ js/noakhli.js
@@ -19,6 +19,17 @@ document
         document.getElementById("noakhali-donation-amount").innerText =
           newDonationAmount;
         document.getElementById("balance-amount").innerText = newBalanceAmount;
+
+        const donationTitle =
+          document.getElementById("noakhli-title").innerText;
+        const historyBox = document.createElement("div");
+        historyBox.classList.add("border", "border-gray-300", "mb-5", "p-3");
+        const currentTime = new Date().toLocaleTimeString();
+        historyBox.innerHTML = `
+      <h3 class="text-lg font-semibold">${donationAmount} Taka is donated for ${donationTitle}</h3>
+    <p>Time: ${currentTime}</p>
+    `;
+        document.getElementById("history-container").appendChild(historyBox);
       }
     }
   });
